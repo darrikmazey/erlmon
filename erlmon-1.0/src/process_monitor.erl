06dbf114ce9256dd8fa59a4fd2d4948782dedaaa
Darrik Mazey <darrik@darmasoft.com>
2010-02-13 19:25:33 -0500

    process_monitor can now take a regexp or pid to monitor

--- erlmon-1.0/src/process_monitor.erl
+++ erlmon-1.0/src/process_monitor.erl
@@ -10,6 +10,16 @@
 start(ProcessName) ->
 	spawn_link(?MODULE, init, [ProcessName]).
 
+init(Pid) when is_integer(Pid) ->
+	debug:log("process_monitor: starting for ~p", [Pid]),
+	Name = ps:name_for_pid(Pid),
+	case Name of
+		null ->
+			state_mon ! #state_change{sender=self(), node=node(), objtype=process, obj=Pid, prev_state=unknown, new_state=not_running, ts=timestamp:now_i()};
+		P ->
+			state_mon ! #state_change{sender=self(), node=node(), objtype=process, obj=P, prev_state=unknown, new_state=running, ts=timestamp:now_i()},
+			loop(lists:flatten(io_lib:format("~p", [Pid])), P, running)
+	end;
 init(ProcessName) ->
 	debug:log("process_monitor: starting for ~p", [ProcessName]),
 	Pid = ps:pid_for_process(ProcessName),
@@ -23,39 +33,51 @@ init(ProcessName) ->
 	end.
 
 loop(Pid, ProcessName, running) ->
-	timer:sleep(3000),
-	Cpid = ps:pid_for_process(ProcessName),
-	case Cpid of
-		null ->
-			state_mon ! #state_change{sender=self(), node=node(), objtype=process, obj=ProcessName, prev_state=running, new_state=not_running, ts=timestamp:now_i()},
-			loop(null, ProcessName, not_running);
-		Pid ->
-			loop(Pid, ProcessName, running);
-		NewPid ->
-			state_mon ! #state_change{sender=self(), node=node(), objtype=process, obj=ProcessName, prev_state=running, new_state=restarted, ts=timestamp:now_i()},
-			loop(NewPid, ProcessName, restarted)
+	receive
+		stop -> stopped
+	after
+		3000 ->
+			Cpid = ps:pid_for_process(ProcessName),
+			case Cpid of
+				null ->
+					state_mon ! #state_change{sender=self(), node=node(), objtype=process, obj=ProcessName, prev_state=running, new_state=not_running, ts=timestamp:now_i()},
+					loop(null, ProcessName, not_running);
+				Pid ->
+					loop(Pid, ProcessName, running);
+				NewPid ->
+					state_mon ! #state_change{sender=self(), node=node(), objtype=process, obj=ProcessName, prev_state=running, new_state=restarted, ts=timestamp:now_i()},
+					loop(NewPid, ProcessName, restarted)
+			end
 	end;
 loop(null, ProcessName, not_running) ->
-	timer:sleep(3000),
-	Cpid = ps:pid_for_process(ProcessName),
-	case Cpid of
-		null ->
-			loop(null, ProcessName, not_running);
-		Pid ->
-			state_mon ! #state_change{sender=self(), node=node(), objtype=process, obj=ProcessName, prev_state=not_running, new_state=running, ts=timestamp:now_i()},
-			loop(Pid, ProcessName, running)
+	receive
+		stop -> stopped
+	after
+		3000 ->
+			Cpid = ps:pid_for_process(ProcessName),
+			case Cpid of
+				null ->
+					loop(null, ProcessName, not_running);
+				Pid ->
+					state_mon ! #state_change{sender=self(), node=node(), objtype=process, obj=ProcessName, prev_state=not_running, new_state=running, ts=timestamp:now_i()},
+					loop(Pid, ProcessName, running)
+			end
 	end;
 loop(Pid, ProcessName, restarted) ->
-	timer:sleep(3000),
-	Cpid = ps:pid_for_process(ProcessName),
-	case Cpid of
-		null ->
-			state_mon ! #state_change{sender=self(), node=node(), objtype=process, obj=ProcessName, prev_state=restarted, new_state=not_running, ts=timestamp:now_i()},
-			loop(null, ProcessName, not_running);
-		Pid ->
-			state_mon ! #state_change{sender=self(), node=node(), objtype=process, obj=ProcessName, prev_state=restarted, new_state=running, ts=timestamp:now_i()},
-			loop(Pid, ProcessName, running);
-		NewPid ->
-			state_mon ! #state_change{sender=self(), node=node(), objtype=process, obj=ProcessName, prev_state=restarted, new_state=restarted, ts=timestamp:now_i()},
-			loop(NewPid, ProcessName, restarted)
+	receive
+		stop -> stopped
+	after
+		3000 ->
+			Cpid = ps:pid_for_process(ProcessName),
+			case Cpid of
+				null ->
+					state_mon ! #state_change{sender=self(), node=node(), objtype=process, obj=ProcessName, prev_state=restarted, new_state=not_running, ts=timestamp:now_i()},
+					loop(null, ProcessName, not_running);
+				Pid ->
+					state_mon ! #state_change{sender=self(), node=node(), objtype=process, obj=ProcessName, prev_state=restarted, new_state=running, ts=timestamp:now_i()},
+					loop(Pid, ProcessName, running);
+				NewPid ->
+					state_mon ! #state_change{sender=self(), node=node(), objtype=process, obj=ProcessName, prev_state=restarted, new_state=restarted, ts=timestamp:now_i()},
+					loop(NewPid, ProcessName, restarted)
+			end
 	end.
--- erlmon-1.0/src/ps.erl
+++ erlmon-1.0/src/ps.erl
@@ -7,6 +7,7 @@
 -export([dump/1]).
 -export([find/1]).
 -export([pid_for_process/1]).
+-export([name_for_pid/1]).
 
 -include("include/erlmon.hrl").
 
@@ -53,8 +54,37 @@ find(Pname, [_H|T]) ->
 find(_Pname, []) ->
 	not_found.
 
+find_regex(Pregex) ->
+	find_regex(Pregex, list()).
+
+find_regex(Pregex, [H|T]) ->
+  #process{cmd=Pname} = H,
+	REReturn = re:run(Pname, Pregex),
+	case REReturn of
+		{match, _} -> H;
+		nomatch -> find_regex(Pregex, T)
+	end;
+find_regex(_Pregex, []) ->
+	not_found.
+
+find_pid(Pid) ->
+	find_pid(Pid, list()).
+
+find_pid(Pid, [H = #process{pid=Pid}|_T]) -> H;
+find_pid(Pid, [_H|T]) -> find_pid(Pid, T);
+find_pid(_Pid, []) -> not_found.
+
 pid_for_process(Pname) ->
 	Process = find(Pname),
+	case Process of
+		not_found -> pid_for_process_regex(Pname);
+		P ->
+			#process{pid=Pid} = P,
+			Pid
+	end.
+
+pid_for_process_regex(Pregex) ->
+	Process = find_regex(Pregex),
 	case Process of
 		not_found -> null;
 		P ->
@@ -62,3 +92,19 @@ pid_for_process(Pname) ->
 			Pid
 	end.
 
+name_for_pid(Pid) when is_integer(Pid) ->
+	Process = find_pid(lists:flatten(io_lib:format("~p", [Pid]))),
+	case Process of
+		not_found -> null;
+		P ->
+			#process{cmd=Cmd} = P,
+			Cmd
+	end;
+name_for_pid(Pid) ->
+	Process = find_pid(Pid),
+	case Process of
+		not_found -> null;
+		P ->
+			#process{cmd=Cmd} = P,
+			Cmd
+	end.
