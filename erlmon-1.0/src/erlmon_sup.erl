db728f2d012a5732f458e01d773a6d98fd1ed294
Darrik Mazey <darrik@darmasoft.com>
2010-03-15 15:22:23 -0400

    remove commented line

--- erlmon-1.0/src/erlmon_sup.erl
+++ erlmon-1.0/src/erlmon_sup.erl
@@ -13,7 +13,6 @@ start_link() ->
 init(_) ->
 	debug:log("erlmon_sup: starting"),
 	NChild = {node_sup, {node_sup, start_link, []}, permanent, 2000, supervisor, [node_sup]},
-	%SMChild = { state_change_sup, {state_change_sup, start_link, []}, permanent, 2000, supervisor, [state_change_sup]},
 	SChild = { storage_sup, {storage_sup, start_link, []}, permanent, 2000, supervisor, [storage_sup]},
 	MSChild = { monitor_sup, {monitor_sup, start_link, []}, permanent, 2000, supervisor, [monitor_sup]},
 	SGChild = { erlmon_smtp_sup, {erlmon_smtp_sup, start_link, []}, permanent, 2000, supervisor, [erlmon_smtp_sup]},
