4598b0af2271906c86f053377d7ef7eec49f3953
Darrik Mazey <darrik@darmasoft.com>
2010-03-15 22:27:20 -0400

    start monitor supervisor before config

--- erlmon-1.0/src/erlmon_sup.erl
+++ erlmon-1.0/src/erlmon_sup.erl
@@ -15,7 +15,7 @@ init(_) ->
 	SMChild = { state_change_sup, {state_change_sup, start_link, []}, permanent, 2000, supervisor, [state_change_sup]},
 	SChild = { storage_sup, {storage_sup, start_link, []}, permanent, 2000, supervisor, [storage_sup]},
 	NChild = { node_sup, {node_sup, start_link, []}, permanent, 2000, supervisor, [node_sup]},
-	CChild = { config_sup, {config_sup, start_link, []}, permanent, 2000, worker, [config_sup]},
 	MSChild = { monitor_sup, {monitor_sup, start_link, []}, permanent, 2000, supervisor, [monitor_sup]},
+	CChild = { config_sup, {config_sup, start_link, []}, permanent, 2000, worker, [config_sup]},
 	SGChild = { erlmon_smtp_sup, {erlmon_smtp_sup, start_link, []}, permanent, 2000, supervisor, [erlmon_smtp_sup]},
-	{ok, {{one_for_one, 1, 1}, [SMChild, SChild, NChild, CChild, MSChild, SGChild]}}.
+	{ok, {{one_for_one, 1, 1}, [SMChild, SChild, NChild, MSChild, CChild,  SGChild]}}.
