da76bfa3098521f4f0883c3fdb8728d21ffaccfd
Darrik Mazey <darrik@darmasoft.com>
2010-03-18 13:13:51 -0400

    complain about missing or inaccessible config

--- erlmon-1.0/src/pages/web_config.erl
+++ erlmon-1.0/src/pages/web_config.erl
@@ -9,15 +9,22 @@ title() ->
 	"erlmon configuration".
 
 body() ->
-  {ok, Config} = file:read_file("/home/darrik/wdir/erlmon/erlmon-1.0/config.lua"),
-	debug:log("Config == ~p", [Config]),
-  [
-    #h1{text="Configuration"},
-    #p{},
-    #label{text="Edit the configuration file for this node."},
-    #textarea { text=binary_to_list(Config), html_encode=false }
+  case file:read_file("/home/darrik/wdir/erlmon/erlmon-1.0/config.lua") of
+		{ok, Config} ->
+			[
+				#h1{text="Configuration"},
+				#p{},
+				#label{text="Edit the configuration file for this node."},
+				#textarea { text=binary_to_list(Config), html_encode=false }
 
-  ].
+			];
+		{error, Reason} ->
+			[
+				#h1{text="Configuration"},
+				#p{},
+				#label{text="config.lua is either missing or inaccessible.  Check the file and try again, please."}
+			]
+	end.
 
 menu_items() -> helper:menu([{home,"dashboard","/"},{nodes,"configuration","/web/config"}]).
 	
