5b2d656271673bc8644f8745ddc1d37910646cd3
Darrik Mazey <darrik@darmasoft.com>
2010-03-18 15:08:41 -0400

    classes for node_status

--- erlmon-1.0/src/pages/web_index.erl
+++ erlmon-1.0/src/pages/web_index.erl
@@ -30,13 +30,18 @@ get_node_status() ->
 	]}.
 
 node_status_to_html([{Node, Status}|T]) ->
+	{Text, Class} = case Status of
+		up ->
+			{"UP", "node_status_up"};
+		down ->
+			{"DOWN", "node_status_down"};
+		_ ->
+			{"UNKNOWN", "node_status_unknown"}
+	end,
 	[
 		#tablerow { cells = [
 			#tablecell { text=atom_to_list(Node) },
-			#tablecell { text=case Status of
-				up -> "UP";
-				down -> "DOWN";
-				_ -> "UNKNOWN" end }
+			#tablecell { text=Text, class=Class }
 		]}
 	| node_status_to_html(T)];
 node_status_to_html([]) ->
